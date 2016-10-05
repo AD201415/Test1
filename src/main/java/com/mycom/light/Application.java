a241119adccff96e6f5a4eb5b1e74629bef66a9a
AD2016 <404834770@qq.com>
2016-10-05 11:43:10 +0800

    HAHAHA

---
+++ src/main/java/com/mycom/light/Application.java
@@ -0,0 +1,14 @@
+package com.mycom.light;
+
+
+
+/**
+ * Created by ADX on 2016/8/18.
+ */
+public class Application {
+    public static void main(String[] args){
+        System.out.println("hello, world");
+        //TODO:fjsdfsdjk
+        System.out.println("Hello Java");
+    }
+}
