327695f5a3013511b0c29ca889daae55752ebc7b
AD2016 <404834770@qq.com>
2016-10-05 14:07:42 +0800

    HAHA

---
+++ src/main/java/com/mycom/light/Test1.java
@@ -0,0 +1,10 @@
+package com.mycom.light;
+
+/**
+ * Created by ADX on 2016/10/5.
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("ABCDEFG");
+    }
+}
